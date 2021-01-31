1ef45cd80b699f93db92b510455bddede4c472fc
huaxk <huaxk@163.com>
2021-01-31 23:47:25 +0800

    Init repo

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/huaxk/hackernews
+
+go 1.15
+
+require (
+	github.com/99designs/gqlgen v0.13.0
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/go-chi/chi v3.3.2+incompatible
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/golang-migrate/migrate/v4 v4.14.1
+	github.com/upper/db/v4 v4.1.0 // indirect
+	github.com/vektah/gqlparser/v2 v2.1.0
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
+)
---
+++ server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi"
+	"github.com/huaxk/hackernews/graph"
+	"github.com/huaxk/hackernews/graph/generated"
+	"github.com/huaxk/hackernews/internal/auth"
+	database "github.com/huaxk/hackernews/internal/pkg/db/mysql"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	router := chi.NewRouter()
+	router.Use(auth.Middleware())
+
+	database.InitDb()
+	// database.Migrate()
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, router))
+}
